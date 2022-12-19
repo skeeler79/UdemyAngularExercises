0936cdc7c499e9adec9ff681c0adb83ea90cd917
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-12-19 12:48:18 -0500

    adds two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  //styles can be written within here and overwrite the css file
+  styles: [`
+  h3 {
+    color: dodgerblue;
+  }`
+    
+  ]
 })
 export class AppComponent {
-  title = 'my-first-app';
+  
 }
