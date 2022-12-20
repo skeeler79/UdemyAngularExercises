598ee308408d6f7fc51d2c3b8a083cb048586ca4
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-12-20 15:10:41 -0500

    finishes assignement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  addParagraph = false;
+  clickArray = [];
+  
+
+  showParagraph(){
+    this.addParagraph = !this.addParagraph;
+    this.clickArray.push(this.clickArray.length +1)
+  }
+
+  changeColor (){
+    if (this.clickArray.length >= 5){
+      return 'blue'
+    }else{
+      return 'white';
+    }
+  
+  }
+  
 }
