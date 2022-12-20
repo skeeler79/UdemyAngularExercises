2364152e59dee81e400606d8d04e2353c3672bf1
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-12-20 15:11:57 -0500

    adds directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online {
+        color: white;
+    }`]
 })
 
 
@@ -10,7 +14,16 @@ export class ServerComponent {
  serverID: number = 10;
  serverStatus: string = 'offline';
 
+ constructor (){
+     this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+     //math.random produces a number between 0 and 1. if the number is greater than .5, change from online to offline.
+}
  getServerStatus(){
      return this.serverStatus;
+ }
+
+ getColor(){
+     return this.serverStatus === 'online' ? 'green' : 'red';
+     //ternary expression if serverStatus is online, return green, otherwise return red. Can also use an if then statement.
  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver2'];
   
   constructor() { 
   setTimeout(() => {
@@ -21,7 +23,10 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer (){
+    this.serverCreated = true; 
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+
   }
 
   onUpdateServerName (words){
